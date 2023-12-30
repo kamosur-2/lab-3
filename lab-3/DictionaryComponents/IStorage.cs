846f9f83baf634098294d4844bc380204ec94498
bushkovsky <62966446+bushkovsky@users.noreply.github.com>
2023-12-30 13:21:57 +0300

    new lab

--- lab-3/DictionaryComponents/IStorage.cs
+++
@@ -1,7 +0,0 @@
-namespace lab_2.DictionaryComponents;
-
-public interface IStorage
-{ 
-    public void AddNewWord(string word, StorageContext storageContext);
-    public IList<Word>? WordSearch(string word);
-}
